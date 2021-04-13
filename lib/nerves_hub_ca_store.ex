8e2cc6eca52256890cb9652c86865c330156b7e4
Jon Carstens <jjcarstens@me.com>
2021-04-13 09:46:36 -0600

    include helper functions and docs

--- lib/nerves_hub_ca_store.ex
+++ lib/nerves_hub_ca_store.ex
@@ -1,16 +1,41 @@
 defmodule NervesHubCaStore do
   @moduledoc """
   Certificate Authority Store for the production NervesHub instance
+
+  #{File.read!("README.md") |> String.split("## Usage") |> Enum.at(1)}
   """
 
-  @doc "Returns DER encoded certs"
-  @spec ca_certs() :: [binary()]
-  def ca_certs do
-    ca_cert_path = Application.app_dir(:nerves_hub_ca_store, ["priv", "ssl", "prod"])
+  @type env :: :prod | :staging
 
-    ca_cert_path
-    |> File.ls!()
-    |> Enum.map(&File.read!(Path.join(ca_cert_path, &1)))
-    |> Enum.map(&X509.Certificate.to_der(X509.Certificate.from_pem!(&1)))
+  @doc """
+  Returns DER encoded list of CA certificates
+  """
+  @spec cacerts(env) :: [:public_key.der_encoded()]
+  def cacerts(env \\ :prod) do
+    for cert <- certificates(env), do: X509.Certificate.to_der(cert)
+  end
+
+  @doc """
+  Alias for NervesHubCaStore.cacerts/1
+  """
+  @spec ca_certs(env) :: [:public_key.der_encoded()]
+  def ca_certs(env \\ :prod), do: cacerts(env)
+
+  @doc """
+  CA Store as list of OTP compatible certificate records
+  """
+  @spec certificates(env) :: [X509.Certificate.t()]
+  def certificates(env \\ :prod) do
+    file_path(env)
+    |> File.read!()
+    |> X509.from_pem()
+  end
+
+  @doc """
+  File path to cacerts.pem
+  """
+  @spec file_path(env :: :prod | :staging) :: Path.t()
+  def file_path(env \\ :prod) do
+    Application.app_dir(:nerves_hub_ca_store, ["priv", "ssl", "#{env}", "cacerts.pem"])
   end
 end
--- test/nerves_hub_ca_store_test.exs
+++ test/nerves_hub_ca_store_test.exs
@@ -2,9 +2,38 @@ defmodule NervesHubCaStoreTest do
   use ExUnit.Case
   doctest NervesHubCaStore
 
-  test "Checks that certs are bundled" do
-    certs = NervesHubCaStore.ca_certs()
-    assert is_list(certs)
-    assert Enum.count(certs) == 3
+  test "production certificate store" do
+    check_path(:prod)
+    check_certificates(:prod)
+    check_bundled(:prod)
+  end
+
+  test "staging certificate store" do
+    check_path(:staging)
+    check_certificates(:staging)
+    check_bundled(:staging)
+  end
+
+  test "alias ca_certs" do
+    der_certs = NervesHubCaStore.ca_certs()
+    assert is_list(der_certs)
+  end
+
+  defp check_bundled(env) do
+    der_certs = NervesHubCaStore.cacerts(env)
+    assert is_list(der_certs)
+    assert length(der_certs) == 3
+    assert Enum.all?(der_certs, &is_binary/1)
+  end
+
+  defp check_path(env) do
+    assert File.exists?(NervesHubCaStore.file_path(env))
+  end
+
+  defp check_certificates(env) do
+    otp_certs = NervesHubCaStore.certificates(env)
+    assert is_list(otp_certs)
+    assert length(otp_certs) == 3
+    assert Enum.all?(otp_certs, &is_tuple/1)
   end
 end
